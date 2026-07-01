f1e8809a17e29a887616d25109872c855556edf4
Dmitriy <disaythis@gmail.com>
2026-07-01 20:59:39 +0300

    feat: добавить поддержку шифрования и расшифровки

--- cmd/playground/main.go
+++ cmd/playground/main.go
@@ -1,10 +1,96 @@
 package main
 
-import "net/http"
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"io"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func mainPage(res http.ResponseWriter, req *http.Request) {
+	res.Write([]byte("Привет!"))
+}
+
+func apiPage(res http.ResponseWriter, req *http.Request) {
+	res.Write([]byte("Это страница /api."))
+}
 
 func main() {
-	err := http.ListenAndServe(`:8080`, nil)
+	err := godotenv.Load()
 	if err != nil {
 		panic(err)
 	}
+	mux := http.NewServeMux()
+	mux.HandleFunc(`/api/`, apiPage)
+	mux.HandleFunc(`/`, mainPage)
+
+	err = http.ListenAndServe(`:8080`, mux)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func sum[T int | float64 | float32](a, b T) T {
+	return a + b
+}
+
+type Encrypter struct {
+	Key string
+}
+
+func NewEncrypter() *Encrypter {
+	key := os.Getenv("KEY")
+	if key == "" {
+		panic("Не передан ENV KEY")
+	}
+
+	return &Encrypter{
+		Key: key,
+	}
+}
+
+func (enc *Encrypter) Encrypt(plainText []byte) ([]byte, error) {
+	block, err := aes.NewCipher([]byte(enc.Key))
+	if err != nil {
+		return nil, err
+	}
+
+	aesGCM, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	nonce := make([]byte, aesGCM.NonceSize())
+
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		return nil, err
+	}
+
+	cipherText := aesGCM.Seal(nonce, nonce, plainText, nil)
+
+	return cipherText, nil
+}
+
+func (enc *Encrypter) Decrypt(encryptedStr []byte) ([]byte, error) {
+	block, err := aes.NewCipher([]byte(enc.Key))
+	if err != nil {
+		return nil, err
+	}
+
+	aesGCM, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	nonceSize := aesGCM.NonceSize()
+	nonce, cipherText := encryptedStr[:nonceSize], encryptedStr[nonceSize:]
+	plainText, err := aesGCM.Open(nil, nonce, cipherText, nil)
+	if err != nil {
+		return nil, err
+	}
+	return plainText, nil
 }
